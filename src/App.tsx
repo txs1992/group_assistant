e340b4079ab3b969db11a4e1aa9905d5cb9d2672
ChrisWen <chriswen96@163.com>
2019-07-30 11:29:00 +0800

    Add title png

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import logo from './assets/logo.svg';
 import './App.scss';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-          Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-          Learn Reacts
-      </a>
-    </header>
-  </div>
-);
+interface AppProps{}
+
+class App extends React.Component<AppProps> {
+  constructor(props:AppProps) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="app">
+      Hello world
+      </div>
+    );
+  }
+}
 
 export default App;
